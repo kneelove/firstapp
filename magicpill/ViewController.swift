16055ce84049335c5bd150def5e945f345f12fca
Nilav Vaghasiya <nilav@Nilavs-Mac.local>
2017-01-21 11:03:59 +0530

    Added UIPicker with two rows with two text field

--- magicpill/ViewController.swift
+++ magicpill/ViewController.swift
@@ -8,10 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
+    
+    
+    var basepill = ["Paracetamol","Diclofam","Duoling"]
+    var toppill = ["Gringo","Yello","Hugo"]
+    
+    @IBOutlet weak var PillButton: UIButton!
+    @IBOutlet weak var PillPicker: UIPickerView!
+    
     override func viewDidLoad() {
+    
+        
         super.viewDidLoad()
+        PillPicker.dataSource=self
+        PillPicker.delegate=self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +31,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func PillButtonPressed(_ sender: Any) {
+        PillPicker.isHidden = false
+    }
 
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return basepill.count
+        
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+  
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0{
+            return basepill[row]
+        }else if component == 1{
+            return toppill[row]
+            
+        }
+        
+        return "Other: \(row)"
+        //if loop for component printing name
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let a = "\(basepill[row]) \(toppill[row])"
+        PillButton.setTitle(a, for: UIControlState.normal)
+        PillPicker.isHidden=true
+    }
+    
 }
+
 
